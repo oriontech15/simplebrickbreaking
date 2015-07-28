727535a65e794136930c9028e44714814e595733
Justin Smith <11jsmith7@gmail.com>
2015-07-28 13:52:32 -0600

    Initial Commit

---
+++ BreakingBricks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BreakingBricks
+//
+//  Created by Justin Smith on 7/28/15.
+//  Copyright (c) 2015 Justin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BreakingBricks/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  BreakingBricks
+//
+
+//  Copyright (c) 2015 Justin Smith. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
